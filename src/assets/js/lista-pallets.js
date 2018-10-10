11cfe5cba24a669f33000290b6d31e84e400dc47
Elton Rodrigues <elton.leite@multilaser.com.br>
2018-10-10 14:12:47 -0300

    atualizado detalhes do código da lista de pallets

--- src/assets/js/lista-pallets.js
+++ src/assets/js/lista-pallets.js
@@ -5,5 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.href = '/login.html';
   });
 
+  let input = document.querySelector('#codigo-pallet');
+  input.addEventListener('input', () => {
+    let items = document.querySelectorAll('.lista-pallet__detalhes-produto');
 
+    if (input.value.length > 0) {
+      items.forEach(item => {
+        let codigo = item.querySelector('.codigo');
+        let regex = new RegExp(input.value, 'i');
+
+        if (!regex.test(codigo.textContent)) {
+          item.classList.add('lista-pallet__detalhes-produto--invisivel');
+        } else {
+          item.classList.remove('lista-pallet__detalhes-produto--invisivel');
+        }
+      });
+    } else {
+      items.forEach(item => item.classList.remove('lista-pallet__detalhes-produto--invisivel'));
+    }
+  });
+
+  let pallets = document.querySelectorAll('.lista-pallet__detalhes-produto');
+  pallets.forEach(pallet => {
+    pallet.addEventListener('click', () => {
+      // Ir para leitura do código de barras
+      window.location.href = '/leitura-codigo-barras.html';
+    });
+  });
 });
