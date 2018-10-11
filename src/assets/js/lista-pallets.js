c20ec816ca4178e8c6a4c4725e4b8261a5fb163f
Elton Rodrigues <elton.leite@multilaser.com.br>
2018-10-11 14:57:32 -0300

    criado o for para alimentar a lista de pallet

--- src/assets/js/lista-pallets.js
+++ src/assets/js/lista-pallets.js
@@ -1,6 +1,45 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
+//   <section class="lista-pallet__detalhes-produto">
+//     <img src="assets/images/pallet.svg" alt="pen drive" class="img-responsive">
+//     <div>
+//        <span>Código do pallet</span>
+//        <p class="codigo">ABCDE445566</p>
+//        <span>Produto</span>
+//        <p>Pen drive Multilaser Twist 2 16GB</p>
+//     </div>
+//   </section>
+  var listaPallet = document.querySelector('.lista-pallet');
+  for (var i = 0; i < 5; i++) {
+    var section = document.createElement('section');
+    var img = document.createElement('img');
+    var div = document.createElement('div');
+    var spanPallet = document.createElement('span');
+    var pPallet = document.createElement('p');
+    var spanProduto = document.createElement('span');
+    var pProduto = document.createElement('p');
+
+    spanPallet.textContent = 'Código do Pallet';
+    pPallet.textContent = 'ABCDE';
+    pPallet.classList.add('codigo');
+    spanProduto.textContent = 'Produto';
+    pProduto.textContent = 'Pen drive';
+    div.appendChild(spanPallet);
+    div.appendChild(pPallet);
+    div.appendChild(spanProduto);
+    div.appendChild(pProduto);
+
+    img.setAttribute('src', 'assets/images/pallet.svg');
+    img.classList.add('img-responsive');
+
+    section.classList.add('lista-pallet__detalhes-produto');
+    section.appendChild(img);
+    section.appendChild(div);
+
+    listaPallet.appendChild(section);
+  }
+
   var close = document.querySelector('.container__title i');
   close.addEventListener('click', function () {
     // Sair
