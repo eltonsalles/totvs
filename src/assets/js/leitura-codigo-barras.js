99c56431f4d62af66487baa4b8985ace6cd01060
Elton Rodrigues <elton.leite@multilaser.com.br>
2018-10-11 10:04:30 -0300

    ajustes no layout

--- src/assets/js/leitura-codigo-barras.js
+++ src/assets/js/leitura-codigo-barras.js
@@ -1,10 +1,12 @@
+let alturaTela = (screen.width) >= 600 ? 160 : 400;
+
 Quagga.init({
   inputStream: {
     name: "Live",
     type: "LiveStream",
     constraints: {
       width: 324,
-      height: 400,
+      height: alturaTela,
       facingMode: "environment"
     }
   },
@@ -23,4 +25,12 @@ Quagga.init({
 
 Quagga.onDetected(function(result) {
   document.querySelector(".found").innerHTML = result.codeResult.code;
+});
+
+document.querySelector('', () => {
+  let codigoBarras = document.querySelector('.found');
+  codigoBarras.addEventListener('input', () => {
+    // Leio o código de barras
+    alert(codigoBarras.textContent);
+  })
 });
